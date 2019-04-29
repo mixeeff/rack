d52c166f22b16355b7d22a5e95e7fedd70b14cf1
mixeeff <dm.mikheev@gmail.com>
2019-04-29 22:46:20 +0300

    less middleware

--- app.rb
+++ app.rb
@@ -1,20 +1,43 @@
+require_relative 'params_parser'
 class App
   def call(env)
-    [ status, headers, body ]
+  	params = get_params(env)
+  	return empty_params unless params
+
+  	parser = ParamsParser.new(params)
+
+  	if parser.valid?
+  		formatted_time(parser.time_format)
+  	else
+  		error_in_params(parser.errors)
+  	end
   end
 
-	private
+  private
 
-	def status
-		200
-	end
-
-	def headers
-    { 'Content-Type' => 'text/plain' }
+  def get_params(env)
+  	Rack::Utils.parse_nested_query(env["QUERY_STRING"])["format"]
   end
 
-	def body
-		[]
-	end
+  def formatted_time(format)
+  	status = 200
+  	body = ["Current time is #{Time.now.strftime(format.join('-'))}"]
+  	[ status, headers, body ]
+  end
 
+  def error_in_params(errors)
+  	status = 400
+  	body = ["Unknown time format [#{errors.join(', ')}]"]
+  	[ status, headers, body ]
+  end
+
+  def empty_params
+    status = 400
+    body = ["Format didn't specified"]
+  	[ status, headers, body ]
+  end
+
+  def headers
+  	{ 'Content-Type' => 'text/plain' }
+  end
 end
--- config.ru
+++ config.ru
@@ -1,7 +1,5 @@
 require_relative 'middleware/url_check'
-require_relative 'middleware/params_parser'
 require_relative 'app'
 
 use URLCheck
-use ParamsParser
 run App.new
